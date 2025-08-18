4fee5f085ea30274fad4d31bbb41c93062160f57
Mirfayzbek Mansurov <mirfayzmansurov016@gmail.com>
2025-08-18 12:19:26 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
-# config.py
-
 BOT_TOKEN = "8461331939:AAFTnBncGUUJA34WUa2NKu-iAAulbymiL1w"
-BOT_USERNAME = "mansurovkonkursbot"
-CHANNEL_USERNAME = "@allgamessavdo"
-ADMIN_IDS = [5708983199]
+
+ADMINS = [5708983199]  # Admin Telegram ID'lari
+
+CHANNELS = [-1002827757434]  # Kanal ID'lari (minus bilan boshlanadi)
+
+REFERAL_BALL = 1  # Har bir referal uchun ball
+
+USERS = {}  # user_id: {"ref": referrer_id, "score": int, "joined": bool}
